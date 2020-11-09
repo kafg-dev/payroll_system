58d370d6de55e312c0075641e59491ab77b7f0da
kafg-dev <54938908+kafg-dev@users.noreply.github.com>
2020-11-09 21:29:11 +0800

    update

--- account/footer.php
+++ account/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-                    © 2020 KAFG <span class="text-muted d-none d-sm-inline-block float-right">E-INFOMEDIA SOLUTIONS</span>
+                    © 2020 KAFG <span class="text-muted d-none d-sm-inline-block float-right">AAA SOLUTIONS</span>
 </footer>
